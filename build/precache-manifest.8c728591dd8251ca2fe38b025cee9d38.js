2bad3d4993d64d4c1d3cf6ac74e18bf6c51c1b4f
Prathamesh <mytjohn7@gmail.com>
2020-11-16 23:40:01 +0530

    added cycle images

---
+++ build/precache-manifest.8c728591dd8251ca2fe38b025cee9d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9a2df5862ea88bafec396d1fd41aa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bb3222e7c2f3917dd9",
+    "url": "/static/css/main.43db99ef.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bb3222e7c2f3917dd9",
+    "url": "/static/js/main.27a78ed5.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
